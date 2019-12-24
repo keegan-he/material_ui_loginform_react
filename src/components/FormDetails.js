d4de97ebab46352a87af6148b27d71abdd4dcb1d
Keegan H <keeganslane@gmail.com>
2019-12-24 15:10:37 -0800

    butt add

--- src/components/FormDetails.js
+++ src/components/FormDetails.js
@@ -15,16 +15,45 @@ class FormDetails extends Component {
       <MultiThemeProvider>
         <React.Fragment>
           <AppBar title="Please Enter User Details" />
-          <TextField 
-          hintText="Enter First Name..."
-          floatingLabelText="First Name"
-          onChange={this.props.handleChange('firstName')}
-          defaultValue={this.props.values.firstName}
+          <TextField
+            hintText="Enter First Name..."
+            floatingLabelText="First Name"
+            onChange={this.props.handleChange('firstName')}
+            defaultValue={this.props.values.firstName}
+          />
+
+          <br />
+
+          <TextField
+            hintText="Enter Last Name..."
+            floatingLabelText="Last Name"
+            onChange={this.props.handleChange('lastName')}
+            defaultValue={this.props.values.lastName}
+          />
+          <br />
+
+          <TextField
+            hintText="Enter Email Address"
+            floatingLabelText="Email"
+            onChange={this.props.handleChange('email')}
+            defaultValue={this.props.values.email}
+          />
+          <br />
+          <RaisedButton
+            label="Continue"
+            secondary={true}
+            styles={styles.button}
           />
         </React.Fragment>
       </MultiThemeProvider>
     );
   }
 }
+
+const styles = {
+  button: {
+    margin: 15
+  }
+};
 
 export default FormDetails;
