48310170041048a3860b9efcc19cd4c17f4e4436
Keegan H <keeganslane@gmail.com>
2019-12-24 15:53:53 -0800

    change List

--- src/components/Confirm.js
+++ src/components/Confirm.js
@@ -7,23 +7,49 @@ import RaisedButton from 'material-ui/RaisedButton';
 class Confirm extends Component {
   continue = e => {
     e.preventDefault();
-
     //Send data to backend here
-
     this.props.nextStep();
   };
 
+  back = e => {
+    e.preventDefault();
+    this.props.previousStep();
+  };
+
   render() {
+    const {
+      values: { firstName, lastName, email, occupation, bio, city }
+    } = this.props;
     return (
       <MultiThemeProvider>
         <React.Fragment>
           <AppBar title="Confirm User Details" />
+          <List>
+            <ListItem primaryText="First Name" secondaryText={firstName} />
+
+            <ListItem primaryText="Last Name" secondaryText={lastName} />
+
+            <ListItem primaryText="Email" secondaryText={email} />
+
+            <ListItem primaryText="Occupatiion" secondaryText={occupation} />
+
+            <ListItem primaryText="City" secondaryText={city} />
+
+            <ListItem primaryText="Bio=" secondaryText={bio} />
+          </List>
           <br />
           <RaisedButton
-            label="Continue"
+            label="Confirm & Continue"
             secondary={true}
             styles={styles.button}
             onClick={this.continue}
+          />
+
+          <RaisedButton
+            label="Back"
+            secondary={false}
+            styles={styles.button}
+            onClick={this.back}
           />
         </React.Fragment>
       </MultiThemeProvider>
