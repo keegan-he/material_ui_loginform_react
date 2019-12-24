fd2fd8e6cbdc8763967151669db65e2cd02b9ef7
Keegan H <keeganslane@gmail.com>
2019-12-24 14:48:39 -0800

    import material

--- src/components/FormDetails.js
+++ src/components/FormDetails.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import MultiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
 export class FormDetails extends Component {
-    render() {
-        return (
-            <div>
-                <h1>hi</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>hi</h1>
+      </div>
+    );
+  }
 }
 
-export default FormDetails
+export default FormDetails;
