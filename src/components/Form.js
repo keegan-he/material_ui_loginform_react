87338f7763ad9f354f673a96d6184d919b408783
Keegan H <keeganslane@gmail.com>
2019-12-24 14:37:02 -0800

    form comp added

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,43 +1,64 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class Form extends Component {
-constructor(props) {
+  constructor(props) {
     super(props);
     state = {
-        step: 1,
-        firstName:'',
-        lastName: '',
-        email: '',
-        ocupation: '',
-        city: '',
-        bio:''
-    }
-}
+      step: 1,
+      firstName: '',
+      lastName: '',
+      email: '',
+      ocupation: '',
+      city: '',
+      bio: ''
+    };
+  }
 
-// Go to next step
-nextStep = () => {
+  // Go to next step
+  nextStep = () => {
     const { step } = this.state;
     this.setState({
-        step: step + 1
-    })
-}
+      step: step + 1
+    });
+  };
 
-// Go back to previous step
-previousStep = () => {
+  // Go back to previous step
+  previousStep = () => {
     const { step } = this.state;
     this.setState({
-        step: step - 1
-    })
-}
+      step: step - 1
+    });
+  };
 
+  // Handle fields change
+  handleChange = input => e => {
+    this.setState({ [input]: e.target.value });
+  };
 
-    render() {
+  render() {
+    const { step } = this.state;
+
+    const { firstName, lastName, email, occupation, city, bio } = this.state;
+
+    const values = { firstName, lastName, email, occupation, city, bio };
+
+    switch (step) {
+      case 1:
         return (
-            <div>
-                
-            </div>
-        )
+          <FormDetails
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+        case 2: 
+            return <h1>PersonalDetails</h1>
+        case 3: 
+            return <h1>Confirm</h1>
+        case 4: 
+            return <h1>Success</h1>
     }
+  }
 }
 
-export default Form
+export default Form;
